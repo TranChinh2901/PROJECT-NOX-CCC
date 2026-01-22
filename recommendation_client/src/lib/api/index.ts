ce440d1752f0805473a8415bc5c01244af6d91d4
Tranchinh2901 <chinh99078@donga.edu.vn>
2026-01-22 21:04:38 +0700

    update code

---
+++ recommendation_client/src/lib/api/index.ts
@@ -0,0 +1,19 @@
+// ĐỊnh hình và gọi api tại đây
+
+// Ví dụ 
+// export const authApi = {
+//   login: async (credentials: LoginDto): Promise<AuthResponse> => {
+//     const response = await apiClient.post('/auth/login', credentials);
+//     return response.data;
+//   },
+  
+//   register: async (userData: RegisterDto): Promise<RegisterResponse> => {
+//     const response = await apiClient.post('/auth/register', userData);
+//     return response.data;
+
+//   },
+  
+//   logout: () => {
+//     return apiClient.post('/auth/logout');
+//   },
+// }
